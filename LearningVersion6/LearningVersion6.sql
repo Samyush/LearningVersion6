4204a487d28d63d014e477f609926bfb7f22e96b
samyush <samyushmaharjan@gmail.com>
2023-02-26 20:54:42 +0545

    sql update

--- LearningVersion6/LearningVersion6.sql
+++ LearningVersion6/LearningVersion6.sql
@@ -4,3 +4,15 @@ SELECT *
 FROM Sales
 
 EXEC FirstStoredProcdure
+
+SELECT ASCII('a')
+
+Select CHAR(65)
+
+Declare @start int
+Set @start = 97
+While (@start <= 122)
+Begin
+	Print CHAR(@start)
+	Set @start = @start + 1
+END
