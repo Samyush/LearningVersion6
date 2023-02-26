84a177121abc70cdd44aceba31a0f2cba59a3f8b
samyush <samyushmaharjan@gmail.com>
2023-02-26 16:54:39 +0545

    adding connection string for both dockerized and windows sql server

--- LearningVersion6/Program.cs
+++ LearningVersion6/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("LearningVersion6DbConnection"),
+        builder.Configuration.GetConnectionString("LearningVersion6DbConnectionWindows"),
         b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
 var app = builder.Build();
